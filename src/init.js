b53853c0a693dadc6e101eb34bfdde8d5320b7fc
4gl <@>
2026-05-15 14:59:44 +0100

    fix: csp compliance

---
+++ src/init.js
@@ -0,0 +1,16 @@
+(function () {
+  var el = document.getElementById("pacman");
+
+  if (Modernizr.canvas && Modernizr.localstorage &&
+    Modernizr.audio && (Modernizr.audio.ogg || Modernizr.audio.mp3)) {
+    window.setTimeout(function () { PACMAN.init(el, "./"); }, 0);
+  } else {
+    var msg = document.createElement("span");
+    msg.textContent = "Sorry, needs a decent browser";
+    var small = document.createElement("small");
+    small.textContent = "(firefox 3.6+, Chrome 4+, Opera 10+ and Safari 4+)";
+    el.appendChild(msg);
+    el.appendChild(document.createElement("br"));
+    el.appendChild(small);
+  }
+}());
